72d097dd1aebda1d543c6c9287a5052b52de4122
Justinas Runevičius <justinas.runevicius@ktu.edu>
2023-12-04 16:42:46 +0200

    Starting code

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://api.chucknorris.io/jokes";
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+  try {
+      const result = await axios.get(API_URL + "/random");
+      const joke = result.data.value;
+      res.render("index.ejs", {joke});
+  } catch (error) {
+      res.render("index.ejs", { joke: "Error fetching Chuck Norris joke" });
+  }
+});
+
+app.post('/category', async (req, res) => {
+  const selectedCategory = req.body.value;
+  console.log(selectedCategory);
+
+  try {
+    const result = await axios.get(API_URL + `/random?category=${selectedCategory}`);
+    const joke = result.data.value;
+    res.render("index.ejs", {joke});
+  } catch (error) {
+      res.render("index.ejs", { joke: "Error fetching Chuck Norris joke" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
