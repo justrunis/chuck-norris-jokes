df68104c23a18cd5cc69f1dbe2d294005ae89edf
Justinas Runevičius <justinas.runevicius@ktu.edu>
2023-12-05 09:19:01 +0200

    code clean and readme update

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # chuck-norris-jokes
 App that use API to show random Chuck Norris Joke
+
+# Installing Required Packages with npm
+
+## Introduction
+
+When working on a Node.js project, you can manage dependencies using npm (Node Package Manager). The `npm i` command is a shorthand for `npm install` and is used to install all the required packages specified in the `package.json` file.
+
+## Steps
+
+1. **Navigate to Your Project Directory:**
+
+2. **Check `package.json` File:**
+
+
+3. **Run `npm i` Command:**
+
+
+4. **Wait for Installation:**
+
+
+5. **Check `node_modules` Directory:**
+
+Once installation is complete, a node_modules directory will be created.
--- index.js
+++ index.js
@@ -9,19 +9,26 @@ const API_URL = "https://api.chucknorris.io/jokes";
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', async (req, res) => {
+async function getRandomJoke(){
   try {
-      const result = await axios.get(API_URL + "/random");
-      const joke = result.data.value;
-      res.render("index.ejs", {joke});
-  } catch (error) {
-      res.render("index.ejs", { joke: "Error fetching Chuck Norris joke" });
-  }
+    const result = await axios.get(API_URL + "/random");
+    const joke = result.data.value;
+    res.render("index.ejs", {joke});
+} catch (error) {
+    res.render("index.ejs", { joke: "Error fetching Chuck Norris joke" });
+}
+}
+
+app.get('/', async (req, res) => {
+  getRandomJoke();
 });
 
 app.post('/category', async (req, res) => {
   const selectedCategory = req.body.value;
   console.log(selectedCategory);
+  if(selectedCategory === ""){
+    getRandomJoke();
+  }
 
   try {
     const result = await axios.get(API_URL + `/random?category=${selectedCategory}`);
